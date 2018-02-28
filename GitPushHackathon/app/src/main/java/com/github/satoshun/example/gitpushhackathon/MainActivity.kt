19c404971f6cc9a6216782fe48c3fa663b3c6bb5
satoshun <shun.sato1@gmail.com>
2018-02-28 07:55:34 +0000

    add library and hoge

--- GitPushHackathon/app/src/main/java/com/github/satoshun/example/gitpushhackathon/MainActivity.kt
+++ GitPushHackathon/app/src/main/java/com/github/satoshun/example/gitpushhackathon/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.github.satoshun.example.gitpushhackathon
 
 import android.os.Bundle
+import android.support.customtabs.CustomTabsIntent
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import androidx.net.toUri
+
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main_act)
+
+    findViewById<View>(R.id.login).setOnClickListener {
+      loginGitHub()
+    }
+  }
+
+  private fun loginGitHub() {
+    val uri = "https://github.com/login/oauth/authorize?client_id=id".toUri()
+    val builder = CustomTabsIntent.Builder()
+    val customTabsIntent = builder.build()
+    customTabsIntent.launchUrl(this, uri)
   }
 }
---
+++ GitPushHackathon/data/src/main/java/com/github/satoshun/example/data/github/github.kt
@@ -0,0 +1,9 @@
+package com.github.satoshun.example.data.github
+
+import kotlinx.coroutines.experimental.Deferred
+import retrofit2.http.GET
+
+interface GitHub {
+  @GET("users/whatever")
+  fun oauth2(): Deferred<Any>
+}
