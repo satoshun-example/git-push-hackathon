176bc2c4555a224dbd16e959d07a4e4c6063393a
satoshun <shun.sato1@gmail.com>
2018-02-28 06:56:54 +0000

    first

---
+++ GitPushHackathon/app/src/main/java/com/github/satoshun/example/gitpushhackathon/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.satoshun.example.gitpushhackathon
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main_act)
+  }
+}
