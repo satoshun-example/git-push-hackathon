557e65abe8cab7e19fd4cb86d11db7d53e31b657
satoshun <shun.sato1@gmail.com>
2018-02-28 10:27:19 +0000

    base architecture but not perfect

--- GitPushHackathon/app/src/main/java/com/github/satoshun/example/gitpushhackathon/feed/FeedActivity.kt
+++ GitPushHackathon/app/src/main/java/com/github/satoshun/example/gitpushhackathon/feed/FeedActivity.kt
@@ -3,10 +3,63 @@ package com.github.satoshun.example.gitpushhackathon.feed
 import android.os.Bundle
 import com.github.satoshun.example.gitpushhackathon.R
 import dagger.android.support.DaggerAppCompatActivity
+import io.reactivex.Flowable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.processors.PublishProcessor
+import io.reactivex.rxkotlin.cast
+import javax.inject.Inject
 
 class FeedActivity : DaggerAppCompatActivity() {
+  @Inject lateinit var creator: FeedActionCreator
+  @Inject lateinit var store: FeedStore
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.feed_act)
+
+    store.newFeeds.observeOn(AndroidSchedulers.mainThread())
+    store.errorFeed.observeOn(AndroidSchedulers.mainThread())
+
+    creator.newFeeds()
   }
+}
+
+class FeedActionCreator @Inject constructor(
+    private val sink: BaseSink<FeedAction>
+) {
+  fun newFeeds() {
+  }
+}
+
+typealias FeedEntryList = FeedAction.FeedEntryList
+typealias FeedError = FeedAction.FeedError
+
+sealed class FeedAction {
+  data class FeedEntryList(val titles: List<String>) : FeedAction()
+  data class FeedError(val error: Throwable) : FeedAction()
+}
+
+interface BaseSource<T> {
+  val actions: Flowable<T>
+}
+
+interface BaseSink<T> {
+  fun dispatch(action: T)
+}
+
+class FeedDispatcher @Inject constructor() :
+    BaseSource<FeedAction>,
+    BaseSink<FeedAction> {
+  override val actions = PublishProcessor.create<FeedAction>()!!
+
+  override fun dispatch(action: FeedAction) {
+    actions.onNext(action)
+  }
+}
+
+class FeedStore @Inject constructor(
+    source: BaseSource<FeedAction>
+) {
+  val newFeeds: Flowable<FeedEntryList> = source.actions.cast()
+  val errorFeed: Flowable<FeedError> = source.actions.cast()
 }
--- GitPushHackathon/app/src/main/java/com/github/satoshun/example/gitpushhackathon/feed/FeedActivityModule.kt
+++ GitPushHackathon/app/src/main/java/com/github/satoshun/example/gitpushhackathon/feed/FeedActivityModule.kt
@@ -1,10 +1,19 @@
 package com.github.satoshun.example.gitpushhackathon.feed
 
+import dagger.Binds
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module
 interface FeedActivityModule {
-  @ContributesAndroidInjector
+  @ContributesAndroidInjector(modules = [
+    FeedActivityBindModule::class
+  ])
   fun contributeFeedActivity(): FeedActivity
+}
+
+@Module
+interface FeedActivityBindModule {
+  @Binds fun bindSource(dispatcher: FeedDispatcher): BaseSource<FeedAction>
+  @Binds fun bindSink(dispatcher: FeedDispatcher): BaseSink<FeedAction>
 }
