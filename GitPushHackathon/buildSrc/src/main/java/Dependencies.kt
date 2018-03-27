bc000cc06673cbc4630f5af1c7b2afbd6ce3e41b
satoshun <shun.sato1@gmail.com>
2018-03-27 23:24:15 +0000

    update libs

--- GitPushHackathon/buildSrc/src/main/java/Dependencies.kt
+++ GitPushHackathon/buildSrc/src/main/java/Dependencies.kt
@@ -1,6 +1,6 @@
 object Versions {
-  val build_gradle = "3.1.0-rc02"
-  val kotlin_version = "1.2.30"
+  val build_gradle = "3.1.0"
+  val kotlin_version = "1.2.31"
 
   val support_lib = "27.1.0"
   val design_lib = "27.1.0"
@@ -9,13 +9,16 @@ object Versions {
   val ktx = "0.2"
 
   val okhttp = "3.10.0"
-  val retrofit = "2.3.0"
+  val retrofit = "2.4.0"
 
   val rxjava = "2.1.10"
   val rxandroid = "2.0.2"
   val rxkotlin = "2.2.0"
   val RxLifecycleOwner = "0.1.0"
 
-  val dagger = "2.14.1"
+  val dagger = "2.15"
   val serialization_version = "0.4.1"
+
+  val robolectric = "3.8"
+  val mockito_kotlin = "mockito-kotlin-kt1.1:1.5.0"
 }
